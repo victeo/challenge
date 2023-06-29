b8b7cd5a5179c890605b981a9dcb1d41cb0851b8
Victor Teodoro <victor.atomo@gmail.com>
2023-06-29 15:02:36 +0100

    Add files via upload
    Manualmente

--- components/weather/Index.jsx
+++ components/weather/Index.jsx
@@ -1,4 +1,34 @@
+"use client";
+import styles from './weather.module.scss';
+
+// import { Button } from "@material-tailwind/react";
+
+import { ThemeProvider, Button } from "@material-tailwind/react";
+
+export { ThemeProvider, Button };
+
+
+import {useEffect} from 'react';
+
+
 export default function Index(props) {
-    console.log(props)
-    return 'ksajdkasjkdjaskjdaksjdask'
+    useEffect(() => {
+        // 👇 add class to body element
+        document.body.classList.add(styles.weather);
+    }, []);
+    console.log(props.info)
+    return (
+        <>
+            <main>
+                <div className={styles.sidebar}>
+
+                    <Button color="red">color blue</Button>
+
+                </div>
+                <div className={styles.content}>
+
+                </div>
+            </main>
+        </>
+    )
 }
