b014a6bd551e00bfa98b1f2de8054fdbca03d757
Victor Teodoro <victor.atomo@gmail.com>
2023-07-01 10:28:30 +0100

    Configs

--- next.config.js
+++ next.config.js
@@ -5,12 +5,23 @@ module.exports = nextConfig
 
 
 const path = require('path');
- 
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./components/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+        "./api/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+});
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     // prependData: `@import "globals.scss";`,
-  },
-  
+  }
 };
 
