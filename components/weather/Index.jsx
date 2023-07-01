5d830083f60475685af6745bfb1289f20084748e
Victor Teodoro <victor.atomo@gmail.com>
2023-07-01 10:29:26 +0100

    weather

--- components/weather/Index.jsx
+++ components/weather/Index.jsx
@@ -1,28 +1,48 @@
 "use client";
 import styles from './weather.module.scss';
-
-// import { Button } from "@material-tailwind/react";
-
-import { ThemeProvider, Button } from "@material-tailwind/react";
-
-export { ThemeProvider, Button };
-
-
+import Button from "../theme/components/Button";
 import {useEffect} from 'react';
-
+import { LuLocateFixed } from "react-icons/lu";
 
 export default function Index(props) {
     useEffect(() => {
         // 👇 add class to body element
         document.body.classList.add(styles.weather);
     }, []);
-    console.log(props.info)
+    const backgroundColor = '#6E707A';
+    const color = 'white';
+    const padding = '11px 18px 10px';
+    const borderRadius = '0';
+    const text = 'Seach for places';
+    const fontSize = '16px'
+
     return (
         <>
             <main>
                 <div className={styles.sidebar}>
-
-                    <Button color="red">color blue</Button>
+                    <div>
+                        <Button
+                            backgroundColor={backgroundColor}
+                            color={color} padding={padding}
+                            borderRadius={borderRadius}
+                            text={text}
+                            fontSize={fontSize}
+                            classes={styles.sidebar_search}
+                            icon={{ name: 'LuLocateFixed', family: 'react-icons/lu' }}
+                        />
+                    </div>
+                    <div>
+                        <Button
+                            backgroundColor={backgroundColor}
+                            color={color}
+                            padding={'8px'}
+                            borderRadius={'50%'}
+                            height={'40px'}
+                            width={'40px'}
+                            classes={styles.sidebar_icon}
+                            icon={{ name: 'LuLocateFixed', family: 'react-icons/lu' }}
+                        />
+                    </div>
 
                 </div>
                 <div className={styles.content}>
