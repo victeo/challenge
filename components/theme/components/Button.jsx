bbafc5190c093385c721e5c9d6065c199e5fbc11
Victor Teodoro <victor.atomo@gmail.com>
2023-07-01 10:30:07 +0100

    Button

---
+++ components/theme/components/Button.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { LuLocateFixed } from 'react-icons/lu';
+
+export default function Button({ text, backgroundColor, color, padding, borderRadius, fontSize, classes, width, height, img, icon }) {
+    const renderIcon = () => {
+        if (icon) {
+            const { name, family } = icon;
+            console.log(icon)
+            if (family === 'react-icons/lu') {
+                if (name === 'LuLocateFixed') {
+                    return <LuLocateFixed />;
+                } else if (name === 'FaUser') {
+                    return <FaUser />;
+                }
+            }
+        }
+
+        return null;
+    };
+
+    return (
+        <button
+            style={{
+                backgroundColor: backgroundColor ?? 'blue',
+                color: color ?? 'black',
+                padding: padding ?? '0',
+                borderRadius: borderRadius ?? '0',
+                fontSize: fontSize ?? '12px',
+                width: width ?? 'auto',
+                height: height ?? 'auto',
+                backgroundImage: img ? `url(${img})` : '',
+            }}
+            className={classes}
+        >
+            {renderIcon()}
+            {text}
+        </button>
+    );
+}
